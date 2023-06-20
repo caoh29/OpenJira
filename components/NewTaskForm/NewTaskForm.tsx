7f35e87efd1a4fc595d5aba1faacd5e0885e4e53
Camilo Ordoñez <cronox20@gmail.com>
2023-06-20 12:22:45 -0500

    now is beautiful xd

--- components/NewTaskForm/NewTaskForm.tsx
+++ components/NewTaskForm/NewTaskForm.tsx
@@ -1,5 +1,5 @@
 import { useForm, SubmitHandler } from "react-hook-form";
-import { Button, OutlinedInput, Box } from '@mui/material';
+import { Button, OutlinedInput, Box, FormHelperText } from '@mui/material';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import AddIcon from '@mui/icons-material/Add';
@@ -10,66 +10,83 @@ type Inputs = {
     description: string,
 };
 
-export default function NewTaskForm () {
-
+export default function NewTaskForm() {
     const [isAdding, setIsAdding] = useState(false);
 
     const { register, handleSubmit, reset, formState } = useForm<Inputs>();
-    const onSubmitForm: SubmitHandler<Inputs> = data => {
-        console.log(data)
+    const onSubmitForm: SubmitHandler<Inputs> = (data) => {
+        console.log(data);
         setIsAdding(false);
     };
 
     useEffect(() => {
         if (formState.isSubmitSuccessful) {
-            reset();
+        reset();
         }
     }, [formState.isSubmitSuccessful, reset]);
 
     return (
-        <Box sx={{marginBottom: 2, paddingX: 1}}>
-            {isAdding ? (
-                <form onSubmit={handleSubmit(onSubmitForm)}>
-                    <OutlinedInput 
-                        {...register("title", { required: true, minLength: 3 })}
-                        placeholder="Title"
-                        fullWidth
-                    />
-                    {formState.errors.title && <span>This field is required! Minimum 3 characters</span>}
+        <Box sx={{ marginBottom: 2, paddingX: 1 }}>
+        {isAdding ? (
+            <form onSubmit={handleSubmit(onSubmitForm)}>
+            <OutlinedInput
+                {...register('title', { required: true, minLength: 3 })}
+                placeholder="Title"
+                fullWidth
+            />
+            {formState.errors.title && (
+                <FormHelperText error>
+                This field is required! Minimum 3 characters
+                </FormHelperText>
+            )}
 
-                    <OutlinedInput 
-                        sx={{margin: '20px 0'}} 
-                        {...register("description", { minLength: 3 })}
-                        placeholder="Description"  
-                        fullWidth
-                    />
-                    {formState.errors.description && <span>This field has a minimum of 3 characters</span>}
+            <OutlinedInput
+                sx={{ margin: '20px 0' }}
+                {...register('description', { minLength: 3 })}
+                placeholder="Description"
+                fullWidth
+            />
+            {formState.errors.description && (
+                <FormHelperText error>
+                This field has a minimum of 3 characters
+                </FormHelperText>
+            )}
 
-                    <Box display="flex" gap={5} justifyContent="center" alignItems="center" width="100%">
-                        <OutlinedInput 
-                            sx={{height: '35px', width: '110px'}} 
-                            type="submit" 
-                            value="SAVE " 
-                            color="primary" 
-                            endAdornment={<SaveOutlinedIcon/>}
-                        />
-                        <Button 
-                            variant="outlined" 
-                            color="secondary" 
-                            endIcon={<CancelOutlinedIcon/>} 
-                            onClick={() => setIsAdding(false)}> 
-                                Cancel 
-                        </Button>
-                    </Box>
-                </form>
-            ): <Button 
-                    sx={{marginBottom: '20px'}} 
-                    startIcon={<AddIcon/>} 
-                    fullWidth 
-                    variant="outlined" 
-                    onClick={() => setIsAdding(true)}> 
-                        Add Task 
-                </Button>}
+            <Box
+                display="flex"
+                gap={5}
+                justifyContent="center"
+                alignItems="center"
+                width="100%"
+            >
+                <OutlinedInput
+                    sx={{ height: '35px', width: '110px' }}
+                    type="submit"
+                    value="SAVE "
+                    color="primary"
+                    endAdornment={<SaveOutlinedIcon />}
+                />
+                <Button
+                    variant="outlined"
+                    color="secondary"
+                    endIcon={<CancelOutlinedIcon />}
+                    onClick={() => setIsAdding(false)}
+                >
+                Cancel
+                </Button>
+            </Box>
+            </form>
+        ) : (
+            <Button
+                sx={{ marginBottom: '20px' }}
+                startIcon={<AddIcon />}
+                fullWidth
+                variant="outlined"
+                onClick={() => setIsAdding(true)}
+            >
+            Add Task
+            </Button>
+        )}
         </Box>
     );
-};
+}
