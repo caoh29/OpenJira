db45e6d25af70329a8db66becc1d710e38bb1657
Camilo Ordoñez <cronox20@gmail.com>
2023-06-20 11:47:32 -0500

    adding validation, errorHandling and interactivity

--- components/NewTaskForm/NewTaskForm.tsx
+++ components/NewTaskForm/NewTaskForm.tsx
@@ -3,10 +3,7 @@ import { Button, OutlinedInput, Box } from '@mui/material';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import AddIcon from '@mui/icons-material/Add';
-import { useState } from "react";
-
-
-
+import { useState, useEffect } from "react";
 
 type Inputs = {
     title: string,
@@ -17,24 +14,32 @@ export default function NewTaskForm () {
 
     const [isAdding, setisAdding] = useState(false);
 
-    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
-    const onSubmitForm: SubmitHandler<Inputs> = data => console.log(data);
+    const { register, handleSubmit, reset, formState } = useForm<Inputs>();
+    const onSubmitForm: SubmitHandler<Inputs> = data => {
+        console.log(data)
+        setisAdding(false);
+    };
+
+    useEffect(() => {
+        if (formState.isSubmitSuccessful) {
+            reset();
+        }
+    }, [formState.isSubmitSuccessful, reset]);
 
     return (
         <Box sx={{marginBottom: 2, paddingX: 1}}>
-            <Button sx={{marginBottom: '20px'}} startIcon={<AddIcon/>} fullWidth variant="outlined" onClick={() => setisAdding(true)}> Add Task </Button>
-            {isAdding && (
+            {isAdding ? (
                 <form onSubmit={handleSubmit(onSubmitForm)}>
-                    <OutlinedInput placeholder="Title" {...register("title")} required fullWidth/>
-                    <OutlinedInput sx={{margin: '20px 0'}} placeholder="Description" {...register("description")} required fullWidth/>
-                    {errors.title && <span>This field is required</span>}
-                    {errors.description && <span>This field is required</span>}
+                    <OutlinedInput placeholder="Title" {...register("title", { required: true, minLength: 3 })} fullWidth/>
+                    {formState.errors.title && <span>This field is required! Minimum 3 characters</span>}
+                    <OutlinedInput sx={{margin: '20px 0'}} placeholder="Description" {...register("description", { minLength: 3 })} fullWidth/>
+                    {formState.errors.description && <span>This field has a minimum of 3 characters</span>}
                     <Box display="flex" gap={5} justifyContent="center" alignItems="center" width="100%">
                         <OutlinedInput sx={{height: '35px', width: '110px'}} type="submit" value="SAVE " color="primary" endAdornment={<SaveOutlinedIcon/>}/>
                         <Button variant="outlined" color="secondary" endIcon={<CancelOutlinedIcon/>} onClick={() => setisAdding(false)}> Cancel </Button>
                     </Box>
                 </form>
-            )}
+            ): <Button sx={{marginBottom: '20px'}} startIcon={<AddIcon/>} fullWidth variant="outlined" onClick={() => setisAdding(true)}> Add Task </Button>}
         </Box>
     );
 };
